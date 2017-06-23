ec60d2a2974d1a63564b1581e405816760a09878
VIDHU NIVED P B <29914@user>
2017-06-23 06:46:39 -0400

    Sorted with function

--- Isort.c
+++ Isort.c
@@ -1,21 +1,26 @@
 //Pogram to Implement Insertion Sort
 #include<stdio.h>
-main()
+void sort(int *a)
 {
-  int i,j,a[7]={4,23,43,38,15,10,30},k,h;
+int i,j,k;
   for(i=1;i<7;i++)
     {
       k=a[i];
       for(j=i-1;(j>=0)&&(k<a[j]);j--)
 	{
 	  a[j+1]=a[j];
-	 
+
 	}
- a[j+1]=k;
-      printf("\nAfter %dth pass\n",i);
+	  a[j+1]=k;
+}
+}
+main()
+{
+  int a[7]={4,23,43,38,15,10,30},h;
+sort(a);
       for(h=0;h<7;h++)
 	printf("%d\t",a[h]);
-    }
+    
 }
 
 
